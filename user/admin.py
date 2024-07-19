377a348e74a4ffcd93785a2cecb8a528d896e1c4
dandibizz <blogseorank1@gmail.com>
2024-07-19 07:58:38 -0700

    first commit

---
+++ user/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import CustomUser
+
+# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'id', 'email',)
+    search_fields = ('username', 'email',)
---
+++ user/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import SignupView, login_page,register_page, UserListView,UserDetailView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+urlpatterns = [
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('signin/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/',login_page, name='login_page'),
+    path('register/',register_page,name='register_page'),
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/<str:id>/', UserDetailView.as_view(), name='user-detail'),
+]
